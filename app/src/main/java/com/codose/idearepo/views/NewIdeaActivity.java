f1de303df915896f0a5af00f0020555a27a88157
Osemwingie Oshodin <osemwingieoshodin@gmail.com>
2020-02-11 05:07:16 +0100

    Set Edit background colors

--- app/src/main/java/com/codose/idearepo/views/NewIdeaActivity.java
+++ app/src/main/java/com/codose/idearepo/views/NewIdeaActivity.java
@@ -1,29 +1,40 @@
 package com.codose.idearepo.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.codose.idearepo.R;
 import com.google.android.material.textfield.TextInputEditText;
 
-public class NewIdeaActivity extends AppCompatActivity {
+public class NewIdeaActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String EDIT_TITLE =
             "com.codose.idearepo.views.EDIT_TITLE";
     public static final String EDIT_DESCRIPTION =
             "com.codose.idearepo.views.EDIT_DESCRIPTION";
     private TextInputEditText title;
     private TextInputEditText description;
+    private ImageView bg_blue;
+    private ImageView bg_pink;
+    private ImageView bg_yellow;
     private Button save_idea;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_idea);
-
+        bg_blue = findViewById(R.id.activity_new_idea_blue);
+        bg_pink = findViewById(R.id.activity_new_idea_pink);
+        bg_yellow = findViewById(R.id.activity_new_idea_yellow);
+        bg_blue.setOnClickListener(this);
+        bg_pink.setOnClickListener(this);
+        bg_yellow.setOnClickListener(this);
         title = findViewById(R.id.activity_new_idea_edit_title);
         description = findViewById(R.id.activity_new_idea_edit_description);
         save_idea = findViewById(R.id.activity_new_idea_save_button);
@@ -33,6 +44,14 @@ public class NewIdeaActivity extends AppCompatActivity {
                 saveIdea();
             }
         });
+    }
+
+
+
+    private void setBackground(int color) {
+        ConstraintLayout constraintLayout = findViewById(R.id.activity_new_idea_bg);
+        constraintLayout.setBackgroundColor(getResources().getColor(color));
+
     }
 
     private void saveIdea() {
@@ -48,5 +67,22 @@ public class NewIdeaActivity extends AppCompatActivity {
             setResult(RESULT_OK, data);
             finish();
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.activity_new_idea_blue:
+                    setBackground(R.color.colorPrimary);
+                    break;
+                case R.id.activity_new_idea_pink:
+                    setBackground(R.color.colorAccent);
+                    break;
+                case R.id.activity_new_idea_yellow:
+                    setBackground(R.color.yellow);
+                    break;
+                default:
+                    break;
+            }
     }
 }
